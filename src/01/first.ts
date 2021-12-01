2f4a8ddc93c4b3b3dcef34615efdc25cac4da0d8
Yohannes <johnbrhane1@gmail.com>
2021-12-01 10:13:53 +0100

    First Advent of Code 2021

--- src/01/first.ts
+++ src/01/first.ts
@@ -4,5 +4,14 @@ import path from 'path'
 const filePath = path.join(__dirname, 'sonar.txt')
 const buffer = fs.readFileSync(filePath)
 const text = buffer.toString()
+const measurements: Array<number> = text.split('\n').map((value) => Number.parseInt(value))
 
-console.log(text)
+let numberOfIncreases = 0
+
+for (let index = 1; index < measurements.length; index++) {
+  if (measurements[index] > measurements[index - 1]) {
+    numberOfIncreases++
+  }
+}
+
+console.log(numberOfIncreases)
